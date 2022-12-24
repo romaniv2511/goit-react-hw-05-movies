602a0c04f395c1272f9f83ba21b3415c8f4612d2
romaniv2511 <romaniv2511@gmail.com>
2022-12-24 12:45:12 +0200

    fix router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,13 +11,10 @@ const Reviews = lazy(() => import('../components/Reviews/Reviews'));
 export const App = () => {
   return (
     <Routes>
-      <Route path="/goit-react-hw-05-movies" element={<SharedLayout />}>
+      <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
-        <Route path="/goit-react-hw-05-movies/movies" element={<Movies />} />
-        <Route
-          path="/goit-react-hw-05-movies/movies/:movieId"
-          element={<MovieDetails />}
-        >
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
           <Route path="cast" element={<Cast />}></Route>
           <Route path="reviews" element={<Reviews />}></Route>
         </Route>
